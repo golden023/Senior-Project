cf63b44d9cd325c8ffa422bc4a3b5d7627baeb60
Stephanie <llmsstephaniesinclair@gmail.com>
2019-09-01 15:47:50 -0400

    main page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NewComponent } from './new/new.component';
+import { AllComponent } from './all/all.component';
+import { EventComponent } from './event/event.component';
+import { GiftComponent } from './gift/gift.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewComponent,
+    AllComponent,
+    EventComponent,
+    GiftComponent
   ],
   imports: [
     BrowserModule,
