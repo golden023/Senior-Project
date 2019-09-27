134e68adbc4cf258bbc0e4576db0fa8afd7905ea
Stephanie Sinclair <sinclair2@mail.usf.edu>
2019-09-27 14:38:44 -0400

    import NgbModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +8,7 @@ import { SaleComponent } from './sale/sale.component';
 import { AllComponent } from './all/all.component';
 import { EventComponent } from './event/event.component';
 import { GiftComponent } from './gift/gift.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';  
 
 @NgModule({
   declarations: [
@@ -22,7 +22,8 @@ import { GiftComponent } from './gift/gift.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
