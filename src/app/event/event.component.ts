742a4a9260df8775e3a93a18397e9377f8deb4df
bazed <dbazemor@mail.usf.edu>
2019-10-20 21:16:58 -0400

    Updated home, event, and sale pages

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { KIT } from '../_models/kit';
+import { KitsService } from '../_services';
+
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.scss']
+  styleUrls: ['./event.component.scss'],
+  providers: [KitsService]
 })
 export class EventComponent implements OnInit {
+  ekits: KIT[];
 
-  constructor() { }
+  constructor(private service: KitsService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getKits();
   }
-
-}
+  getKits(): void {
+    this.service.getEventKits()
+      .subscribe(ekits => {
+        return (this.ekits = ekits);
+      });
+  };
+} 
