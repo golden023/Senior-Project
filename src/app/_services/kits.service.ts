deea7f59e0ee3f6e22963dab2295adc2e74d1d6d
Stephanie Sinclair <sinclair2@mail.usf.edu>
2019-11-11 16:43:39 -0500

    logo+favicon+png images

--- src/app/_services/kits.service.ts
+++ src/app/_services/kits.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 
 
-import { KIT } from '../_models/kit';
+import { KIT, Kit } from '../_models/kit';
 
 
 @Injectable({
@@ -12,40 +12,47 @@ import { KIT } from '../_models/kit';
 })
 
 export class KitsService {
-  
-  constructor(private http: HttpClient){}
 
-  getAll (): Observable<KIT[]>  {
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<KIT[]> {
     return this.http.get<KIT[]>('http://localhost:5000/kits')
       .pipe();
   }
 
-  getEventKits (): Observable<KIT[]>  {
+  getEventKits(): Observable<KIT[]> {
     return this.http.get<KIT[]>('http://localhost:5000/eventkits')
       .pipe();
   }
 
-  getGiftKits (): Observable<KIT[]>  {
+  getGiftKits(): Observable<KIT[]> {
     return this.http.get<KIT[]>('http://localhost:5000/giftkits')
       .pipe();
   }
 
-  getSaleKits (): Observable<KIT[]>  {
+  getSaleKits(): Observable<KIT[]> {
     return this.http.get<KIT[]>('http://localhost:5000/salekits')
       .pipe();
   }
 
-  getTopKits (): Observable<KIT[]>  {
+  getTopKits(): Observable<KIT[]> {
     return this.http.get<KIT[]>('http://localhost:5000/topkits')
       .pipe();
   }
 
-  getKit (kitID: string): Observable<KIT[]>  {
+  getKit(kitID: string): Observable<KIT[]> {
     const options = kitID ?
       { params: new HttpParams().set('kitID', kitID) } : {};
 
     return this.http.get<KIT[]>(`http://localhost:5000/singlekit`, options)
       .pipe();
+  }
+
+  sort() {
+    this.getTopKits
+      .sort((a: Kit, b: Kit) => {
+        return a.price.valueOf() - b.price.valueOf();
+      })
   }
 }
 
